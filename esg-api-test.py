ca4afb330d799fcf8b606c6918555c0249eeebcc
Jennifer Mickel <wmtrackinfo@gmail.com>
2022-03-10 18:28:16 -0600

    saving changes

--- esg-api-test.py
+++ esg-api-test.py
@@ -1,14 +1,13 @@
 import http.client
-conn = http.client.HTTPSConnection("tf689y3hbj.execute-api.us-east-1.amazonaws.com/prod/authorization/")
-
-#esg-environmental-social-governance-data.p.rapidapi.com")
+conn = http.client.HTTPSConnection("esg-environmental-social-governance-data.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-host': "esg-environmental-social-governance-data.p.rapidapi.com",
     'x-rapidapi-key': "32df9c2991e6cf6e3407fe9dd88eea0a"
     }
 
-conn.request("GET", "/goals?q=company%20name%20or%20stock%20symbols%20or%20exchange%3Asymbol", headers=headers)
+conn.request("GET", 
+"/goals?q=company%20name%20or%20stock%20symbols%20or%20exchange%3Asymbol", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
